216aa2776f374faf6fa7d3a095af3eb2e45f8c3d
Kenneth L <86786290+ugsfume@users.noreply.github.com>
2024-07-19 17:44:01 +0100

    increased track width

--- fsd_path_planning/config.py
+++ fsd_path_planning/config.py
@@ -117,8 +117,8 @@ def get_default_matching_kwargs(mission: MissionTypes) -> KwargsType:
         The created cone matching kwargs
     """
     return dict(
-        min_track_width=5,
-        max_search_range=6,
+        min_track_width=8,
+        max_search_range=8.5,
         max_search_angle=np.deg2rad(50),
         matches_should_be_monotonic=True,
     )
